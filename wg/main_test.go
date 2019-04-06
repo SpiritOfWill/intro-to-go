369a8070c7bd5e9afeb3351d58fb8709f47be419
SpiritOfWill <14132203+SpiritOfWill@users.noreply.github.com>
2019-04-06 20:32:46 +0300

    disable log output

--- wg/main_test.go
+++ wg/main_test.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"io/ioutil"
+	"log"
 	"testing"
 )
 
 var requests = r(Count)
+
+func init() {
+	log.SetOutput(ioutil.Discard)
+}
 
 func Benchmark_doAsync(b *testing.B) {
 	for n := 0; n < b.N; n++ {
