33571410e9f1ad3b5c021c271ec65d62a948269d
SpiritOfWill <14132203+SpiritOfWill@users.noreply.github.com>
2019-04-08 13:58:58 +0300

    added test

--- wg/main_test.go
+++ wg/main_test.go
@@ -4,6 +4,8 @@ import (
 	"io/ioutil"
 	"log"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 var requests = r(Count)
@@ -21,5 +23,23 @@ func BenchmarkDoAsync(b *testing.B) {
 func BenchmarkDoSync(b *testing.B) {
 	for n := 0; n < b.N; n++ {
 		DoSync(requests)
+	}
+}
+
+func Test_md5sum(t *testing.T) {
+	tests := []struct {
+		req  string
+		want string
+	}{
+		{"abc", "900150983cd24fb0d6963f7d28e17f72"},
+		{"abcd", "e2fc714c4727ee9395f324cd2e7f331f"},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.req, func(t *testing.T) {
+			got := md5sum([]byte(tt.req))
+
+			require.Equal(t, tt.want, got)
+		})
 	}
 }
